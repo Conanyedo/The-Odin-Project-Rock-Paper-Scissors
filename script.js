6a02cacf9d978c506844dee5157c76f6daeb412b
Younes Bouddou <ybouddou@e1r7p15.1337.ma>
2022-07-03 19:33:12 +0100

    manipulate DOM play the game with buttons

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const rockBtn = document.querySelector('.rock-btn');
+const paperBtn = document.querySelector('.paper-btn');
+const scissorsBtn = document.querySelector('.scissors-btn');
+const btns = document.querySelectorAll('button');
+const results = document.querySelector('.result');
+const playerRes = document.querySelector('.player-res');
+const computerRes = document.querySelector('.computer-res');
+
 function computerPlay()
 {
 	const moves = ['Rock', 'Paper', 'Scissors'];
@@ -7,7 +15,7 @@ function computerPlay()
 
 function playRound(playerSelection, computerSelection)
 {
-	console.log(`You chose ${playerSelection}, Computer chose ${computerSelection}!`);
+	// console.log(`You chose ${playerSelection}, Computer chose ${computerSelection}!`);
 	if (playerSelection === computerSelection)
 		return `Draw`;
 	if (playerSelection === 'rock' && computerSelection === 'paper')
@@ -26,30 +34,63 @@ function playRound(playerSelection, computerSelection)
 		return `You Win! Rock beats Scissors`;
 }
 
-function game()
-{
-	let playerSelection, computerSelection, result;
-	let playerScore = 0;
-	let computerScore = 0;
 
-	for (let i = 0; i < 5; i++)
-	{
-		playerSelection = prompt('Choose your move: ').toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
+let result = '';
+let computerSelection;
+let playerSelection;
+
+// Event listeners
+btns.forEach((btn) => {
+	btn.addEventListener('click', function(event) {
 		computerSelection = computerPlay().toLowerCase();
+		playerSelection = event.target.value;
 		result = playRound(playerSelection, computerSelection);
-		console.log(result);
+		results.textContent = result;
 		if (result.search('Lose') >= 0)
 			computerScore++;
 		else if (result.search('Win') >= 0)
 			playerScore++;
-	}
+		if (playerScore === 5 || computerScore === 5)
+		{
+			playerScore = computerScore = 0;
+			playerRes.textContent = playerScore;
+			computerRes.textContent = computerScore;
+		}
+		playerRes.textContent = playerScore;
+		computerRes.textContent = computerScore;
+	});
+});
 
-	console.log(`Your score is: ${playerScore}`);
-	console.log(`Computer's score is: ${computerScore}`);
-	if (playerScore > computerScore)
-		console.log('%c You Win!!!', 'color: green;');
-	else if (playerScore < computerScore)
-		console.log('%c You Lose!!!', 'color: red;');
-	else
-		console.log('%c Draw!!', 'color: blue;');
-}
+
+
+
+
+// function game()
+// {
+// 	let playerSelection, computerSelection, result;
+// 	let playerScore = 0;
+// 	let computerScore = 0;
+
+// 	for (let i = 0; i < 5; i++)
+// 	{
+// 		playerSelection = prompt('Choose your move: ').toLowerCase();
+// 		computerSelection = computerPlay().toLowerCase();
+// 		result = playRound(playerSelection, computerSelection);
+// 		console.log(result);
+// 		if (result.search('Lose') >= 0)
+// 			computerScore++;
+// 		else if (result.search('Win') >= 0)
+// 			playerScore++;
+// 	}
+
+// 	console.log(`Your score is: ${playerScore}`);
+// 	console.log(`Computer's score is: ${computerScore}`);
+// 	if (playerScore > computerScore)
+// 		console.log('%c You Win!!!', 'color: green;');
+// 	else if (playerScore < computerScore)
+// 		console.log('%c You Lose!!!', 'color: red;');
+// 	else
+// 		console.log('%c Draw!!', 'color: blue;');
+// }
